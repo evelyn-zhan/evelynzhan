317bb937abacc37bf0131eaac094e76eabb9bad4
Evelyn <evelynzanpx@gmail.com>
2024-07-29 19:48:23 +0700

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './components/App';
+import './style/input.css';
+import './style/output.css';
+
+const root = document.getElementById('root');
+
+createRoot(root).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);

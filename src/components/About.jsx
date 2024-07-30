95c822e0e43b385a680767164c906cc0974df339
Evelyn <evelynzanpx@gmail.com>
2024-07-30 19:39:44 +0700

    Update: About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,28 +14,28 @@ export default function About({ darkMode }) {
                     <p>☁️ A dreamer and an achiever</p>
                 </div>
 
-                <nav className="mt-6 flex flex-row gap-x-8 text-lg font-medium overflow-x-scroll lg:overflow-x-auto">
-                    <Link to="/about/overview" className={`${location.pathname === "/" || location.pathname === "/about/overview" ? (darkMode ? 'text-primary font-bold' : 'text-darker font-bold') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center trasnition-all ease-in-out duration-600`}>
+                <nav className="mt-6 py-1 flex flex-row gap-x-6 lg:gap-x-10 text-lg font-semibold overflow-x-scroll lg:overflow-x-auto">
+                    <Link to="/about/overview" className={`${location.pathname === "/" || location.pathname === "/about/overview" ? (darkMode ? 'text-primary' : 'text-darker') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center trasnition-all ease-in-out duration-600`}>
                         <i className="ri-search-line"></i>
                         <span>Overview</span>
                     </Link>
 
-                    <Link to="/about/education" className={`${location.pathname === "/about/education" ? (darkMode ? 'text-primary font-bold' : 'text-darker font-bold') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
+                    <Link to="/about/education" className={`${location.pathname === "/about/education" ? (darkMode ? 'text-primary' : 'text-darker') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
                         <i className="ri-graduation-cap-line"></i>
                         <span>Education</span>
                     </Link>
 
-                    <Link to="/about/experience" className={`${location.pathname === "/about/experience" ? (darkMode ? 'text-primary font-bold' : 'text-darker font-bold') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
+                    <Link to="/about/experience" className={`${location.pathname === "/about/experience" ? (darkMode ? 'text-primary' : 'text-darker') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
                         <i className="ri-briefcase-line"></i>
                         <span>Experience</span>
                     </Link>
 
-                    <Link to="/about/achievements" className={`${location.pathname === "/about/achievements" ? (darkMode ? 'text-primary font-bold' : 'text-darker font-bold') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
+                    <Link to="/about/achievements" className={`${location.pathname === "/about/achievements" ? (darkMode ? 'text-primary' : 'text-darker') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
                         <i className="ri-trophy-line"></i>
                         <span>Achievements</span>
                     </Link>
 
-                    <Link to="/about/certification" className={`${location.pathname === "/about/certification" ? (darkMode ? 'text-primary font-bold' : 'text-darker font-bold') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
+                    <Link to="/about/certification" className={`${location.pathname === "/about/certification" ? (darkMode ? 'text-primary' : 'text-darker') : (darkMode ? 'hover:text-primary' : 'hover:text-darker')} flex flex-row gap-x-2 items-center transition-all ease-in-out duration-600`}>
                         <i className="ri-award-line"></i>
                         <span>Certifications</span>
                     </Link>

9d844d7b780376314ba47efa12d8325dec1b444d
Evelyn <evelynzanpx@gmail.com>
2024-07-30 10:21:06 +0700

    Fix: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ export default function Header({ darkMode, changeMode }) {
     return (
         <header className="px-16 py-10">
             <nav className="flex flex-row justify-end items-center gap-x-10 text-xl font-medium">
-                <Link to="/" className={`${currentPath.pathname === "/" ? (darkMode ? 'text-primary' : 'text-darker') : ''} hover:font-bold hover:underline transition-all ease-in-out duration-600`}>About</Link>
-                <Link to="/project" className={`${currentPath.pathname === "/project" ? 'text-primary' : ''} hover:font-bold hover:underline transition-all ease-in-out duration-600`}>Projects</Link>
-                <Link to="/contact" className={`${currentPath.pathname === "/contact" ? 'text-primary' : ''} hover:font-bold hover:underline transition-all ease-in-out duration-600`}>Contact</Link>
+                <Link to="/" className={`${currentPath.pathname === "/" ? (darkMode ? 'text-primary' : 'text-darker') : ''} hover:font-bold transition-all ease-in-out duration-600`}>About</Link>
+                <Link to="/project" className={`${currentPath.pathname === "/project" ? (darkMode ? 'text-primary' : 'text-darker') : ''} hover:font-bold transition-all ease-in-out duration-600`}>Projects</Link>
+                <Link to="/contact" className={`${currentPath.pathname === "/contact" ? (darkMode ? 'text-primary' : 'text-darker') : ''} hover:font-bold transition-all ease-in-out duration-600`}>Contact</Link>
                 {
                     darkMode === false
                     ?   <div className="cursor-pointer px-2.5 py-1.5 bg-darker rounded-md border-solid border-[1.5px] border-darker text-white hover:bg-white hover:text-darker transition-all ease-in-out duration-600" onClick={changeMode}>

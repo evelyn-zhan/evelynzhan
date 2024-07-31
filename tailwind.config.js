0fc927e278e7e65c8616827021b5ba44e1bbc9aa
Evelyn <evelynzanpx@gmail.com>
2024-07-31 10:13:52 +0700

    Update Configuration for TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         'dark-mode': '#1E1E1E',
+        'black': '#000000',
         'white': '#FFFFFF',
+        'grey': '#A9A9A9',
         'darker': '#277CA5',
         'primary': '#64C2EC',
       }

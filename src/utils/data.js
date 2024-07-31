8238ac85c698a711c91d8911b9261888fa23731e
Evelyn <evelynzanpx@gmail.com>
2024-07-31 20:20:10 +0700

    Add: Data file

---
+++ src/utils/data.js
@@ -0,0 +1,40 @@
+const skills = [
+    {
+        id: 1,
+        name: "HTML5",
+        logo: "ri-html5-fill",
+        colorCode: "#FF5733"
+    },
+    {
+        id: 2,
+        name: "CSS3",
+        logo: "ri-css3-fill",
+        colorCode: "#3C99DC"
+    },
+    {
+        id: 3,
+        name: "TailwindCSS",
+        logo: "ri-tailwind-css-fill",
+        colorCode: "#06b6d4"
+    },
+    {
+        id: 4,
+        name: "JavaScript",
+        logo: "ri-javascript-fill",
+        colorCode: "#f7df1e"
+    },
+    {
+        id: 5,
+        name: "ReactJS",
+        logo: "ri-reactjs-line",
+        colorCode: "#61DBFB"
+    },
+    {
+        id: 6,
+        name: "NodeJS",
+        logo: "ri-nodejs-line",
+        colorCode: "#3c873a"
+    }
+]
+
+export default skills

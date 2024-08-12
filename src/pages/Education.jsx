db1200dcab9939f3345a161270fb9f4ad1807a66
Evelyn <evelynzanpx@gmail.com>
2024-08-12 10:16:03 +0700

    Update: About > Education page

--- src/pages/Education.jsx
+++ src/pages/Education.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { education } from '../utils/data';
+import EducationComponent from '../components/EducationComponent.jsx';
+
 export default function Education({ darkMode }) {
     return (
         <div>
             <h2 className={`text-xl lg:text-2xl font-extrabold ${darkMode ? 'text-primary' : 'text-darker'}`}>Formal Education 📖</h2>
-            <div></div>
+            <div className="mt-8 flex flex-col gap-y-8">
+                {
+                    education.map(edu => {
+                        return <EducationComponent key={edu.id} {...edu} darkMode={darkMode} />
+                    })
+                }
+            </div>
         </div>
     )
 }

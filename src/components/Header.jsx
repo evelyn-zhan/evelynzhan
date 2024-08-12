4a369cbf5e8e803e48ddd10f5609721a44fe1f7c
Evelyn <evelynzanpx@gmail.com>
2024-08-12 10:15:08 +0700

    Update: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,17 @@ export default function Header({ darkMode, changeMode }) {
     const currentPath = useLocation()
 
     return (
-        <header className="px-3.5 lg:px-20 py-4 lg:py-6 block sticky top-0 backdrop-blur">
-            <nav className="flex flex-row justify-end items-center gap-x-7 lg:gap-x-10 text-lg lg:text-lg font-medium">
+        <header className="px-3.5 lg:px-24 py-4 block sticky top-0 backdrop-blur">
+            <nav className="flex flex-row justify-end items-center gap-x-7 lg:gap-x-10 text-base lg:text-lg font-medium">
                 <Link to="/" className={`${currentPath.pathname === "/" || currentPath.pathname === "/about/overview" || currentPath.pathname === "/about/education" || currentPath.pathname === "/about/experience" || currentPath.pathname === "/about/achievements" || currentPath.pathname === "/about/certification" ? (darkMode ? 'text-primary' : 'text-darker') : ''} hover:font-bold transition-all ease-in-out duration-600`}>About</Link>
                 <Link to="/project" className={`${currentPath.pathname === "/project" ? (darkMode ? 'text-primary' : 'text-darker') : ''} hover:font-bold transition-all ease-in-out duration-600`}>Projects</Link>
                 <Link to="/contact" className={`${currentPath.pathname === "/contact" ? (darkMode ? 'text-primary' : 'text-darker') : ''} hover:font-bold transition-all ease-in-out duration-600`}>Contact</Link>
                 {
                     darkMode === false
-                    ?   <div className="cursor-pointer px-2 lg:px-2.5 py-1 lg:py-1.5 bg-white rounded-md border-solid border-[1.5px] border-darker text-darker hover:bg-darker hover:text-white transition-all ease-in-out duration-600" onClick={changeMode}>
+                    ?   <div className="cursor-pointer px-2 py-1 bg-white rounded-md border-solid border-[1.5px] border-darker text-darker hover:bg-darker hover:text-white transition-all ease-in-out duration-600" onClick={changeMode}>
                             <i className="ri-moon-line cursor-pointer"></i>
                         </div>
-                    :   <div className="cursor-pointer px-2 lg:px-2.5 py-1 lg:py-1.5 bg-darker rounded-md border-solid border-[1.5px] border-darker text-white hover:bg-white hover:border-white hover:text-darker transition-all ease-in-out duration-600" onClick={changeMode}>
+                    :   <div className="cursor-pointer px-2 py-1 bg-darker rounded-md border-solid border-[1.5px] border-darker text-white hover:bg-white hover:border-white hover:text-darker transition-all ease-in-out duration-600" onClick={changeMode}>
                             <i className="ri-sun-line cursor-pointer"></i>
                         </div>
                 }

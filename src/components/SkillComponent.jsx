f491a0f5c6b669c05f79a7a56d7785cf91204250
Evelyn <evelynzanpx@gmail.com>
2024-08-29 09:55:34 +0700

    Fix: font size

--- src/components/SkillComponent.jsx
+++ src/components/SkillComponent.jsx
@@ -4,7 +4,7 @@ export default function SkillComponent({ logo, colorCode, skill, darkMode }) {
     return (
         <div className={`px-3.5 py-2 w-60 lg:w-52 flex flex-row items-center gap-x-2.5 border-solid border-[1px] rounded-md ${darkMode ? 'border-primary' : 'border-darker'}`}>
             <i className={`${logo} text-lg lg:text-xl text-[${colorCode}]`}></i>
-            <span>{skill}</span>
+            <span className="text-base">{skill}</span>
         </div>
     )
 }

5aba9a49ea399c5473cf8781f9ccfe27968ddd8c
Evelyn <evelynzanpx@gmail.com>
2024-08-29 10:10:42 +0700

    update: use svg for skill logo

--- src/components/SkillComponent.jsx
+++ src/components/SkillComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default function SkillComponent({ logo, colorCode, skill, darkMode }) {
+export default function SkillComponent({ svg, skill, darkMode }) {
     return (
         <div className={`px-3.5 py-2 w-60 lg:w-52 flex flex-row items-center gap-x-2.5 border-solid border-[1px] rounded-md ${darkMode ? 'border-primary' : 'border-darker'}`}>
-            <i className={`${logo} text-lg lg:text-xl text-[${colorCode}]`}></i>
+            {/* <i className={`${logo} text-lg lg:text-xl text-[${colorCode}]`}></i> */}
+            {svg}
             <span className="text-base">{skill}</span>
         </div>
     )
